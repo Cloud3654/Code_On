2add0817db4485a6980c7c8c21452424e8cc07eb
Cloud3654 <163625776+Cloud3654@users.noreply.github.com>
2025-05-04 13:09:18 +0900

    Update 1531: 알파벳 모음 개수 세기.cpp

--- 1531: 알파벳 모음 개수 세기.cpp
+++ 1531: 알파벳 모음 개수 세기.cpp
@@ -1,9 +1,10 @@
 #include <iostream>
+#include <cstring>
 using namespace std;
 int main() {
-    string mm = "aeiouAEIOU";
+    string mm = "aeiou";
     string s; getline(cin, s);
-    int cnt;
-    for (auto k : s) if(mm.find(k) != string::npos) cnt++;
+    int cnt = 0;
+    for (auto k : s) if(mm.find(tolower(k)) != string::npos) cnt++;
     cout << cnt;
 }
